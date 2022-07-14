902667c8205094f28998aa1b0ea952688b8ec012
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-14 12:37:42 -0400

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by Oscar Cristaldo on 2022-07-14.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
