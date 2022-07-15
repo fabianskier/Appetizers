10f8da7813cd96f4ff4268001c759413082453f9
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-15 00:14:05 -0400

    CO-0002: Add appetizer list view
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- Appetizers/Model/Appetizer.swift
+++ Appetizers/Model/Appetizer.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Appetizer: Codable {
+struct Appetizer: Codable, Identifiable {
     let id: Int
     let name: String
     let description: String
@@ -23,14 +23,44 @@ struct AppetizerResponse {
 }
 
 struct MockData {
-    static let sampleAppetizer = Appetizer(id: 1,
-                                           name: "Appetizer name sample",
-                                           description: "Appetizer description sample",
-                                           price: 9.99,
-                                           imageURL: "",
-                                           calories: 99,
-                                           protein: 11,
-                                           carbs: 5)
+    static let sampleAppetizerOne = Appetizer(id: 1,
+                                           name: "Asian Flank Steak",
+                                           description: "This perfectly thin cut just melts in your mouth.",
+                                           price: 8.99,
+                                           imageURL: "asian-flank-steak",
+                                           calories: 300,
+                                           protein: 14,
+                                           carbs: 0)
     
-    static let appetizers = [sampleAppetizer, sampleAppetizer, sampleAppetizer, sampleAppetizer]
+    static let sampleAppetizerTwo = Appetizer(id: 2,
+                                           name: "Blackened Shrimp",
+                                           description: "Seasoned shrimp from the depths of the Atlantic Ocean.",
+                                           price: 6.99,
+                                           imageURL: "blackened-shrimp",
+                                           calories: 450,
+                                           protein: 4,
+                                           carbs: 3)
+    
+    static let sampleAppetizerThree = Appetizer(id: 3,
+                                           name: "Buffalo Chicken Bites",
+                                           description: "The tasty bites of chicken have just the right amount of kick to them.",
+                                                price: 7.49,
+                                           imageURL: "buffalo-chicken-bites",
+                                           calories: 800,
+                                           protein: 5,
+                                           carbs: 22)
+    
+    static let sampleAppetizerFour = Appetizer(id: 4,
+                                           name: "Chicken Avocado Spring Roll",
+                                           description: "These won't last 10 seconds once they hit the table.",
+                                               price: 12.99,
+                                           imageURL: "chicken-avocado-spring-roll",
+                                           calories: 480,
+                                           protein: 7,
+                                           carbs: 19)
+    
+    static let appetizers = [sampleAppetizerOne,
+                             sampleAppetizerTwo,
+                             sampleAppetizerThree,
+                             sampleAppetizerFour]
 }
