8a2141515c547fa42e50c4b40387fed0c3185905
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-15 15:29:53 -0400

    CO-0004: Handle error using alerts and add loading indicator
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

---
+++ Appetizers/ViewModel/AppetizerListViewModel.swift
@@ -0,0 +1,43 @@
+//
+//  AppetizerListViewModel.swift
+//  Appetizers
+//
+//  Created by Oscar Cristaldo on 2022-07-15.
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    
+    @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
+    @Published var isLoading: Bool = false
+    
+    func getAppetizers() {
+        self.isLoading = true
+        
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                
+                self.isLoading = false
+                
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                    
+                case .failure(let error):
+                    switch error {
+                    case .invalidURL:
+                        self.alertItem = AlertContext.invalidURL
+                    case .invalidResponse:
+                        self.alertItem = AlertContext.invalidResponse
+                    case .invalidData:
+                        self.alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        self.alertItem = AlertContext.unableToComplete
+                    }
+                }
+            }
+        }
+    }
+}
