8621f0173ec7e02cf36ad7e81ec822f727b1e6a9
Oscar Cristaldo <fabianskier@icloud.com>
2022-07-18 17:44:44 -0400

    CO-0013: Add persistence with AppStorage
    Signed-off-by: Oscar Cristaldo <fabianskier@icloud.com>

--- Appetizers/Utils/Alerts/Alert.swift
+++ Appetizers/Utils/Alerts/Alert.swift
@@ -48,5 +48,15 @@ struct AlertContext {
                   message: Text("Please ensure your email is correct."),
                   dismissButton: .default(Text("Ok")))
     
+    static let userSaveSuccess =
+        AlertItem(title: Text("Profile Saved"),
+                  message: Text("Your profile information was successfully saved."),
+                  dismissButton: .default(Text("Ok")))
+    
+    static let invalidUserData =
+        AlertItem(title: Text("Profile Error"),
+                  message: Text("There was an error saving or retrieving your profile."),
+                  dismissButton: .default(Text("Ok")))
+    
 }
 
